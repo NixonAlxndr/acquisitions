5fd18a39dbf6b8eb1f86a148c46a1e34afb6e1e0
NIXON ALEXANDER SIMON SURYA KUDUS <nixon.kudus@binus.edu>
2025-09-17 21:45:28 +0700

    Set up neon with drizzle

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  role: varchar({ length: 50 }).notNull().default("user"),
+  created_at: timestamp().defaultNow().notNull(),
+  updated_at: timestamp().defaultNow().notNull()
+});
